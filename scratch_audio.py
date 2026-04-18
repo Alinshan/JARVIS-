2f67c6218a5b8f56301250122fc8f8c8bcf4f6f0
shaan.403 <alinshan2255@gmail.com>
2026-04-18 13:59:59 +0530

    J.A.R.V.I.S - Clean up repository 🧹

--- scratch_audio.py
+++
@@ -1,7 +0,0 @@
-import sounddevice as sd
-import time
-def callback(*args):
-    pass
-with sd.InputStream(samplerate=16000, channels=1, callback=callback):
-    time.sleep(1)
-print("Sounddevice Test OK")
